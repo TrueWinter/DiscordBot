b41a81bce4e2e7c78646935ccf12c5df7601a9c8
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-07-20 17:30:43 +0200

    I think I got this embed right

--- commands/stats.js
+++ commands/stats.js
@@ -5,14 +5,13 @@ require("moment-duration-format");
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
   message.channel.send({ embed: {
-    description: `= STATISTICS =
-    • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-    •   Uptime     :: ${duration}
-    • Users      :: ${client.users.size.toLocaleString()}
-    • Servers    :: ${client.guilds.size.toLocaleString()}
-    • Channels   :: ${client.channels.size.toLocaleString()}
-    • Discord.js :: v${version}
-    • Node       :: ${process.version}`, {code: "asciidoc"});
+    title: "= STATISTICS =" fields [{ name: "• Mem Usage"   value: "${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB"}, +
+    {name: "•   Uptime"    value: "${duration}"}, +
+    {name: "• Users "     value: "${client.users.size.toLocaleString()}"}, +
+    {name: "• Servers" value: "${client.guilds.size.toLocaleString()}"}, +
+    {name: "• Channels" value: "${client.channels.size.toLocaleString()}"}, +
+    {name: "• Discord.js" value: "v${version}"}, +
+    {name: "• Node" value: "${process.version}"}, ] }});
   }
 }
 };
