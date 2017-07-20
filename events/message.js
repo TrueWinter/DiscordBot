b95112aa2d8b9ff0ccc3e419598a6f169f37c769
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-07-20 16:40:47 +0200

    Broke it trying to find a fix

--- events/message.js
+++ events/message.js
@@ -37,7 +37,7 @@ module.exports = (client, message) => {
   // If the command exists, **AND** the user has permission, run it.
   if(cmd && level >= cmd.conf.permLevel) {
     if(cmd.conf.enabled === true) {
-      message.guild.channels.find('name', 'mod-log').send(message.author.tag + " (" + message.author.id + ") ran command: `" + message.content + "` in " + message.channel.name + " (" + message.channel.id + ")").catch ((e) => { console.log(e)}).then ((e) => {
+      message.guild.channels.find('name', 'mod-log').send(message.author.tag + " (" + message.author.id + ") ran command: `" + message.content + "` in " + message.channel.name + " (" + message.channel.id + ")").then ((e) => {
         client.log("log", `${message.guild.name}/#${message.channel.name}:${message.author.username} (${message.author.id}) ran command ${cmd.help.name}`, "CMD");
         console.log(e);
       });
