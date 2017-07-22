2b2d306200f8b3131bf249f7d7d2d7722605185b
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-07-22 18:35:27 +0200

    Updates to messageDelete event logging

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,5 +1,5 @@
 module.exports = (client, message) => {
   configFile = require('../config.json');
-  client.log("log", `${message.author.tag} (${message.author.id}) deleted their message in ${message.guild.name} (${message.guild.id})/#${message.channel.name} (${message.channel.id}) that had the content of \`${message.content}\``, "Message Deleted");
-  message.guild.channels.find('name', configFile.defaultSettings.modLogChannel).send(`${message.author.tag} (${message.author.id}) deleted their message in ${message.channel.name} (${message.channel.id}) that had the content of \`${message.content}\``)
+  client.log("log", ` Message sent by ${message.author.tag} (${message.author.id}) sent in ${message.guild.name} (${message.guild.id})/#${message.channel.name} (${message.channel.id}) that had the content of \`${message.content}\` was deleted`, "Message Deleted");
+  message.guild.channels.find('name', configFile.defaultSettings.modLogChannel).send(`Message sent by ${message.author.tag} (${message.author.id}) in ${message.channel.name} (${message.channel.id}) that had the content of \`${message.content}\` was deleted`)
 };
