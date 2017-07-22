032cddd88acd342546d30de27c89d841d316033a
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-07-22 18:25:06 +0200

    Fixed module.export (at line 1) of messageDelete event

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,4 +1,4 @@
-module.exports = async client => {
+module.exports = (client, message) => {
   configFile = require('../config.json');
   client.log(`${message.author.tag} (${message.author.id}) deleted their message in ${message.server.name} (${message.server.id})/#${message.channel.name} (${message.channel.id}) that had the content of \`${message.content}\``);
   message.guild.channels.find('name', configFile.defaultSettings.modLogChannel).send(`${message.author.tag} (${message.author.id}) deleted their message in ${message.channel.name} (${message.channel.id}) that had the content of \`${message.content}\``)
