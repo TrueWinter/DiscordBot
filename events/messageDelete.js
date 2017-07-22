8f8ccb0fe85f78488d8598ec8dfffc1165637683
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-07-22 18:27:03 +0200

    Fixed messageDelete event file

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,5 +1,5 @@
 module.exports = (client, message) => {
   configFile = require('../config.json');
-  client.log(`${message.author.tag} (${message.author.id}) deleted their message in ${message.server.name} (${message.server.id})/#${message.channel.name} (${message.channel.id}) that had the content of \`${message.content}\``);
+  client.log(`${message.author.tag} (${message.author.id}) deleted their message in ${message.guild.name} (${message.guild.id})/#${message.channel.name} (${message.channel.id}) that had the content of \`${message.content}\``);
   message.guild.channels.find('name', configFile.defaultSettings.modLogChannel).send(`${message.author.tag} (${message.author.id}) deleted their message in ${message.channel.name} (${message.channel.id}) that had the content of \`${message.content}\``)
 };
