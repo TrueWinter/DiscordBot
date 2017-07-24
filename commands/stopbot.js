55f80fd074a87d9dda5731b6877c570f03614be3
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-07-24 19:20:08 +0200

    Small change with the stopbot command

--- commands/stopbot.js
+++ commands/stopbot.js
@@ -2,11 +2,11 @@ exports.run = async (client, message, args, level) => {
 	if (message.channel.type === "dm") {
 		 message.channel.send("This is not a command that can be used in a direct message.");
 	} else {
-		
+
 			message.channel.send(message.author + ", Bot has been stopped")
 			.then((err) => {
 				console.log("Bot stopped by " + message.author.username + "#" + message.author.discriminator + " (" + message.author.id + ")");
-				process.exit(1);
+				process.exit(0);
 			});
 		}
 };
