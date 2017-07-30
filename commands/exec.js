e7df05b299768ae8019eab3bb97128832fa2a505
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-07-30 12:01:05 +0200

    Changes to exec command

--- commands/exec.js
+++ commands/exec.js
@@ -1,9 +1,19 @@
-const exec = require('child_process');
+const { exec } = require('child_process');
 exports.run = async (client, message, args, level) => {
   const code = args.join(" ");
   try {
-    const evaled = exec(code);
-    const clean = await client.clean(client, evaled);
+    const execCommand = exec(code, (error, stdout, stderr) => {
+      const clean = await client.clean(client, execCommand);
+      message.channel.send(`\`\`\`xl\n${clean}\n\`\`\``);
+      if (error) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, error)}\n\`\`\``);
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    });
+    const clean = await client.clean(client, execCommand);
     message.channel.send(`\`\`\`xl\n${clean}\n\`\`\``);
   } catch(err) {
     message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
