fc90d97c56a53b29e89fdb594e6b11e910529c81
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-07-31 17:30:13 +0200

    Trying embeds

--- commands/stats.js
+++ commands/stats.js
@@ -1,17 +1,20 @@
 const { version } = require("discord.js");
 const moment = require("moment");
 require("moment-duration-format");
+const Discord = require("discord.js");
 
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-  message.channel.send(`= STATISTICS =
-• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Uptime     :: ${duration}
-• Users      :: ${client.users.size.toLocaleString()}
-• Servers    :: ${client.guilds.size.toLocaleString()}
-• Channels   :: ${client.channels.size.toLocaleString()}
-• Discord.js :: v${version}
-• Node       :: ${process.version}`, {code: "asciidoc"});
+  const embed = new Discord.RichEmbed()
+  .setTitle("BOT STATS")
+  .addField(`• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`)
+  .addField(`• Uptime     :: ${duration}`)
+  .addField(`• Users      :: ${client.users.size.toLocaleString()}`)
+  .addField(`• Servers    :: ${client.guilds.size.toLocaleString()}`)
+  .addField(`• Channels   :: ${client.channels.size.toLocaleString()}`)
+  .addField(`• Discord.js :: v${version}`)
+  .addField(`• Node       :: ${process.version}`)
+  message.channel.send(embed);
 };
 
 exports.conf = {
