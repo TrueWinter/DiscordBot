70b8e604b53aca6b7319ad5acf73fc2fb59b9384
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-09-03 11:33:02 +0200

    Added forceban command

--- commands/forceban.js
+++ commands/forceban.js
@@ -3,24 +3,24 @@ exports.run = async (client, message, args, level) => {
 	const Discord = require("discord.js");
 		let member = args[0];
 		if(isNaN(member))
-			return message.reply("Please give a user ID to unban");
+			return message.reply("Please give a user ID to ban");
 
 		let reason = args.slice(1).join(' ');
 		if(!reason)
-			return message.reply("Please indicate a reason for the unban!");
+			return message.reply("Please indicate a reason for the ban!");
 
-		message.guild.unban(member)
-		.catch(error => message.reply(`Sorry, I couldn't unban because of : ${error}`));
-		message.reply(`${member} has been unbanned by ${message.author.tag} because: ${reason}`);
+		message.guild.ban(member)
+		.catch(error => message.reply(`Sorry, I couldn't ban because of : ${error}`));
+		message.reply(`${member} has been banned by ${message.author.tag} because: ${reason}`);
 		const embed = new Discord.RichEmbed()
     .setColor("RED")
-    .setTitle("User unbanned")
+    .setTitle("User force banned")
     .addField(`User`, `${member}`, true)
     .addField(`Moderator`, `${message.author.tag} (${message.author.id})`, true)
     .addField(`Reason`, `${reason}`, true);
     message.guild.channels.find('name', configFile.defaultSettings.modLogChannel).send({embed})
     .then(() => {
-      client.log("log", `${message.guild.name}/#${message.channel.name} (${message.channel.id}): ${member} was unbanned by ${message.author.tag} (${message.author.id})`, "CMD");
+      client.log("log", `${message.guild.name}/#${message.channel.name} (${message.channel.id}): ${member} was force banned by ${message.author.tag} (${message.author.id})`, "CMD");
       })
       .catch((err) => {
         console.log(err);
@@ -35,8 +35,8 @@ exports.conf = {
 };
 
 exports.help = {
-  name: "unban",
+  name: "forceban",
   category: "Moderation",
-  description: "Unbans a user",
-  usage: "unban [user ID] [reason]"
+  description: "Bans a user by user ID",
+  usage: "forceban [user ID] [reason]"
 };
