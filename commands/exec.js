919e9f20088f3fc2bbdd1b42fbf3c689f89c7273
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-10-12 16:33:39 +0200

    Version 3.1.3: Fixed exec command output

--- commands/exec.js
+++ commands/exec.js
@@ -1,14 +1,10 @@
 const { exec } = require("child_process");
 
 exports.run = async (client, message, args) => {
-  const command = args.join(" ");
-  const output = await message.channel.send(`Running \`${command}\`...`);
-  let stdOut = await doExec(command).catch(data=> outErr(output, data));
-  stdOut = stdOut.substring(0, 1800);
-  output.edit(`\`OUTPUT\`
-\`\`\`sh
-${client.clean(stdOut)}
-\`\`\``);
+  exec(`${args.join(' ')}`, (error, stdout) => {
+      const response = (error || stdout);
+      message.channel.send(`\`OUTPUT\` \n\`\`\`${response}\`\`\``, {split: true}).catch(console.error);
+    });
 };
 
 exports.conf = {
@@ -23,21 +19,4 @@ exports.help = {
   category: 'System',
   description: 'Executes a console command.',
   usage: 'exec [command]'
-};
-
-const outErr = (message, stdData) => {
-  let { stdout, stderr } = stdData;
-  stderr = stderr ? ["`STDERR`","```sh",client.clean(stderr.substring(0, 700)) || " ","```"] : [];
-  stdout = stdout ? ["`STDOUT`","```sh",client.clean(stdout.substring(0, stderr ? stderr.length : 2046 - 40)) || " ","```"] : [];
-  let msg = stdout.concat(stderr).join("\n").substring(0, 2000);
-  message.edit(msg);
-};
-
-const doExec = (cmd, opts = {}) => {
-  return new Promise((resolve, reject) => {
-    exec(cmd, opts, (err, stdout, stderr) => {
-      if (err) return reject({ stdout, stderr });
-      resolve(stdout);
-    });
-  });
 };
