105cbb0492c636b427cbafc7f957a43a467eb5a0
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-10-16 18:13:12 +0200

    Update exec.js

--- commands/exec.js
+++ commands/exec.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args) => {
   message.reply(`Running command \`${args.join(' ').replace('`', '\`')}\`... Please wait.`);
   var execProcess = exec(`${args.join(' ')}`, (error, stdout) => {
       const response = (error || stdout);
-      console.log(`${message.author.tag} (${message.author.id}) is ran console command: \`${args.join(' ')\``);
+      console.log(`${message.author.tag} (${message.author.id}) is ran console command: \`${args.join(' ')}\``);
       message.channel.send(`\`OUTPUT\` \n\`\`\`${response}\`\`\``, {split: true}).catch(console.error);
     });
     setTimeout(function() { 
