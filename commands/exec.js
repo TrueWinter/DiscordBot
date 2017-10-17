03d68e05165244730443caa761ed5aba57aec5d3
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-10-17 16:52:28 +0200

    Maybe this will work

--- commands/exec.js
+++ commands/exec.js
@@ -4,9 +4,24 @@ exports.run = async (client, message, args) => {
   message.reply(`Running command \`${args.join(' ').replace('`', '\`')}\`... Please wait.`);
   var execProcess = exec(`${args.join(' ')}`, (error, stdout) => {
       const response = (error || stdout);
-      console.log(`${message.author.tag} (${message.author.id}) ran console command: \`${args.join(' ')}\``);
-      message.channel.send(`\`OUTPUT\` \n\`\`\`\n${endOutput}\`\`\``, {split: true}).catch(console.error);
-    });    
+       console.log(`${message.author.tag} (${message.author.id}) ran console command: \`${args.join(' ')}\``);
+       if (response > 1800) {
+         var chunks = [];
+ 
+         for (var i = 0, charsLength = response.length; i < charsLength; i += 1800) {
+             chunks.push("```" + response.replace('`', '\`').substring(i, i + 1800) + "```");
+         }
+ 
+         console.log(chunks); 
+         var endOutput = "";
+         message.channel.send(`\`OUTPUT\`);
+         for (var c = 0; c < chunks.length; c++) {
+           //endOutput += chunks[i];
+           message.channel.send(`\`\`\`\n${chunks[c]}\`\`\``,).catch(console.error);
+         }
+         //message.channel.send(`\`OUTPUT\` \n\`\`\`\n${endOutput}\`\`\``,).catch(console.error);
+       }
+     });   
 };
 
 exports.conf = {
