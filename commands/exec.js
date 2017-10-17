fb75a01705fce2a01e359690cef54445b12a31ea
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-10-17 16:55:27 +0200

    Update exec.js

--- commands/exec.js
+++ commands/exec.js
@@ -21,7 +21,7 @@ exports.run = async (client, message, args) => {
          }
          //message.channel.send(`\`OUTPUT\` \n\`\`\`\n${endOutput}\`\`\``,).catch(console.error);
        } else {
-         message.channel.send(`\`OUTPUT\` \n\`\`\`\n${endOutput}\`\`\``,).catch(console.error);
+         message.channel.send(`\`OUTPUT\` \n\`\`\`\n${response}\`\`\``,).catch(console.error);
        }
      });   
 };
