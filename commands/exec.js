3928074194588b6685a2af2320666da13e3dc524
NdT3Development <nicholis.dutoit+alt1@gmail.com>
2017-10-17 16:13:53 +0200

    Update exec.js

--- commands/exec.js
+++ commands/exec.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
   var execProcess = exec(`${args.join(' ')}`, (error, stdout) => {
       const response = (error || stdout);
       console.log(`${message.author.tag} (${message.author.id}) is ran console command: \`${args.join(' ')}\``);
-      message.channel.send(`\`OUTPUT\` \n\`\`\`${response.replace('`', '\`')}\`\`\``, {split: true}).catch(console.error);
+      message.channel.send(`\`OUTPUT\` \n\`\`\`\n${response.replace('`', '\`')}\`\`\``, {split: true}).catch(console.error);
     });
     
 };
