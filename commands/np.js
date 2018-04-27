7670ae0c39e1b006c11d038b9d135fe269521978
donkeymonkey1 <32933322+donkeymonkey1@users.noreply.github.com>
2018-04-27 18:12:36 +0000

    Update np.js

--- commands/np.js
+++ commands/np.js
@@ -2,13 +2,13 @@
 const Discord = require('discord.js');
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
 	const serverQueue = client.musicQueue.get(message.guild.id);
+	if (client.config.musicEnabled !== 'true') return message.channel.send('Music commands are disabled');
 	if (!serverQueue) return message.channel.send('There is nothing playing.');
 	let embed = new Discord.RichEmbed()
 	    .setColor('RANDOM')
 		.setTitle('Now Playing')
 		.setDescription(serverQueue.songs[0].title)
 		.setFooter(client.user.username, client.user.avatarURL)
-  if (client.config.musicEnabled !== 'true') return message.channel.send('Music commands are disabled');
 	  else return message.channel.send(embed)
 };
 
