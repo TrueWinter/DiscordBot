ce647872e2d7f940cb565ddac093532e34789bfb
donkeymonkey1 <32933322+donkeymonkey1@users.noreply.github.com>
2018-04-27 18:21:37 +0000

    Update np.js

--- commands/np.js
+++ commands/np.js
@@ -1,15 +1,15 @@
 // Code from: https://github.com/iCrawl/Music-Bot
 const Discord = require('discord.js');
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-	const serverQueue = client.musicQueue.get(message.guild.id);
 	if (client.config.musicEnabled !== 'true') return message.channel.send('Music commands are disabled');
+	const serverQueue = client.musicQueue.get(message.guild.id);
 	if (!serverQueue) return message.channel.send('There is nothing playing.');
 	let embed = new Discord.RichEmbed()
 	    .setColor('RANDOM')
 		.setTitle('Now Playing')
 		.setDescription(serverQueue.songs[0].title)
 		.setFooter(client.user.username, client.user.avatarURL)
-	  else return message.channel.send(embed)
+	  return message.channel.send(embed)
 };
 
 exports.conf = {
