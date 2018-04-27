bdb26b5acb8dfd5ff42cf7716c73d62a21e0f567
BuildTools <unconfigured@null.spigotmc.org>
2018-04-27 20:05:33 +0200

    Made embeds for some things

--- commands/level.js
+++ commands/level.js
@@ -1,6 +1,12 @@
+const Discord = require('discord.js');
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const scoreLevel = client.points.get(`${message.guild.id}-${message.author.id}`).level || 0;
-  !scoreLevel ? message.channel.send('You have no levels yet.') : message.channel.send(`You are currently level ${scoreLevel}!`);
+	const scoreLevel = client.points.get(`${message.guild.id}-${message.author.id}`).level || 0;
+	let embed = new Discord.RichEmbed()
+	.setColor('GREEN')
+	.setAuthor('You are currently')
+	.setDescription(scoreLevel)
+	.setFooter(client.user.username, client.user.avatarURL)
+  !scoreLevel ? message.channel.send('You have no levels yet.') : message.channel.send(embed);
 };
 
 exports.conf = {
--- commands/points.js
+++ commands/points.js
@@ -1,6 +1,12 @@
+const Discord = require('discord.js');
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const scorePoints = client.points.get(`${message.guild.id}-${message.author.id}`).points || 0;
-  !scorePoints ? message.channel.send('You have no points yet.') : message.channel.send(`You have ${scorePoints} point${scorePoints !== 1 ? 's' : ''}!`);
+	const scorePoints = client.points.get(`${message.guild.id}-${message.author.id}`).points || 0;
+	let embed = new Discord.RichEmbed()
+	.setColor('GREEN')
+	.setAuthor('You currently have')
+	.setDescription(scorePoints + ' points')
+	.setFooter(client.user.username, client.user.avatarURL)
+  !scorePoints ? message.channel.send('You have no points yet.') : message.channel.send(embed);
 };
 
 exports.conf = {
