53e6d484c1d676e9a3acd317c420f7f99f15daaf
Chinese Marc <42821305+ChineseMarc@users.noreply.github.com>
2018-10-17 17:41:58 +0200

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -4,13 +4,13 @@ exports.run = async (client, message, args) => {
   const {caseNumber} = require('../modules/caseNumber.js');
 
   const user = message.mentions.users.first();
-  const modlog = message.guild.channels.find('name', guildSettings.modLogChannel);
-  if (!modlog) return message.reply('Could not find mod log channel');
+  const modlog = message.guild.channels.find(channel => channel.name === guildSettings.modLogChannel);
+  //if (!modlog) return message.reply('Could not find mod log channel');
   const caseNum = await caseNumber(client, modlog);
   const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${guildSettings.prefix}reason ${caseNum} <reason>.`;
 
   var muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted');
-  if (!modlog) return message.reply('I cannot find a mod log channel');
+  //if (!modlog) return message.reply('I cannot find a mod log channel');
   if (!muteRole) {
     message.guild.createRole({name: 'Muted', color: 'DARKER_GREY', permissions: 36766720}).then(() => message.reply('As there was no `Muted` role, one was created')).catch(()=> {return message.reply('Unable to create `Muted` role that was not already present')});
     muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted');
@@ -29,13 +29,13 @@ exports.run = async (client, message, args) => {
     message.guild.member(user).removeRole(muteRole).then(() => {
       message.channel.send(`User unmuted by ${message.author.tag} (${message.author.id}) with reason \`${reason}\``);
       var unmuteEmbed = embed.setTitle('User Unmuted');
-      modlog.send({embed: unmuteEmbed}).catch(console.error);
+      if (modlog) modlog.send({embed: unmuteEmbed}).catch(console.error);
     });
   } else {
     message.guild.member(user).addRole(muteRole).then(() => {
       message.channel.send(`User muted by ${message.author.tag} (${message.author.id}) with reason \`${reason}\``);
       var muteEmbed = embed.setTitle('User Muted');
-      modlog.send({embed: muteEmbed}).catch(console.error);
+      if (modlog) modlog.send({embed: muteEmbed}).catch(console.error);
     });
   }
 
